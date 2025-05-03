d1286712b2557d9527e824857a08b8ea54727bf9
Priyanshu Anand <anandpriyanshu6@gmail.com>
2025-05-03 16:06:08 +0530

    Added Home Page In Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,12 @@ import { authGuard } from './guards/auth.guard';
 import { GlobalConstants } from './constants/global-constants';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ManageTransactionsComponent } from './components/manage-transactions/manage-transactions.component';
+import { ManageBooksComponent } from './components/manage-books/manage-books.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -16,6 +19,17 @@ export const routes: Routes = [
       {
         path: 'manageTransactions',
         component: ManageTransactionsComponent,
+        canActivate: [authGuard],
+        data: {
+          allowedRoles: [
+            GlobalConstants.NORMAL_ROLE_NAME,
+            GlobalConstants.ADMIN_ROLE_NAME,
+          ],
+        },
+      },
+      {
+        path: 'manageBooks',
+        component: ManageBooksComponent,
         canActivate: [authGuard],
         data: {
           allowedRoles: [
