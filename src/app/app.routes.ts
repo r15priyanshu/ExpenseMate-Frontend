94199c79ef5a423ce662516f34f638c9ddcef711
Priyanshu Anand <anandpriyanshu6@gmail.com>
2025-05-12 02:06:36 +0530

    Added About Component and Used routerLinkActiveOptions To Style Both Parent and Child Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,17 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ManageTransactionsComponent } from './components/manage-transactions/manage-transactions.component';
 import { ManageBooksComponent } from './components/manage-books/manage-books.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
   {
     path: 'dashboard',
     component: DashboardComponent,
     children: [
+      { path: '', redirectTo: 'manageTransactions', pathMatch: 'full'},
       {
         path: 'manageTransactions',
         component: ManageTransactionsComponent,
